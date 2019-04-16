57a3d88926da34be74ff622584a8124d07284e12
Matt Gorski <matt.gorski@gmail.com>
2019-04-16 19:26:14 -0400

    Add android.boot t210 args to porg

--- include/configs/p3450-porg.h
+++ include/configs/p3450-porg.h
@@ -53,13 +53,27 @@
 #define CONFIG_USB_FASTBOOT_BUF_SIZE	  0x40000000
 #define CONFIG_USB_FASTBOOT_BUF_ADDR	  (NV_PA_SDRAM_BASE + 0x10000000)
 
-#define CONFIG_PREBOOT
-
+/* Android bootimg support */
+#define CONFIG_CMD_BOOTA
+#define CONFIG_ANDROID_BOOT_IMAGE
+#define CONFIG_CMD_BOOTA_BOOT_PART	      "LNX"
+#define CONFIG_CMD_BOOTA_RECOVERY_PART	  "SOS"
+#define CONFIG_CMD_BOOTA_DT_PART	      "DTB"
+#define CONFIG_ANDROID_DT_HDR_BUFF	      (NV_PA_SDRAM_BASE + 0x03000000)
+#define CONFIG_ANDROID_BOOT_HDR_BUFF	  (NV_PA_SDRAM_BASE + 0x04000000)
 #define BOARD_EXTRA_ENV_SETTINGS \
-	"preboot=if test -e mmc 1:1 /u-boot-preboot.scr; then " \
-		"load mmc 1:1 ${scriptaddr} /u-boot-preboot.scr; " \
-		"source ${scriptaddr}; " \
-	"fi\0"
+	"bootargs_append=" \
+	"init=init androidboot.secureos=tlk earlycon=uart8250,mmio32,0x70006000 " \
+	"androidboot.modem=none androidboot.serialno=0323317065547 " \
+	"androidboot.security=none tegraid=21.1.2.0.0 ddr_die=3072M@2048M " \
+	"section=512M memtype=0 vpr_resize usb_port_owner_info=1 lane_owner_info=0 " \
+	"emc_max_dvfs=1 touch_id=0@63 video=tegrafb no_console_suspend=1 console=ttyS0,115200n8 " \
+	"debug_uartport=lsport,2 earlyprintk=uart8250-32bit,0x70006000 maxcpus=4 "\
+	'lp0_vec=0x1000@0xff780000 nvdumper_reserved=0xff180000 core_edp_mv=1075 " \
+	"core_edp_ma=4000 tegra_fbmem=0x800000@0x92cb7000 androidboot.touch_panel_id=63  " \
+	"androidboot.touch_feature=0 androidboot.orientation=0 usbcore.old_scheme_first=1 " \
+	"is_hdmi_initialised=1 android.kerneltype=normal androidboot.touch_vendor_id=0 " \
+	"androidboot.bootloader=00.00.2018.01-t210-430913d9 androidboot.verifiedbootstate=orange"
 
 /* General networking support */
 
